58f96cea502a9291fd140a5a39e0b56119800975
Pranav437 <pranavece58@gmail.com>
2021-11-14 23:18:47 -0500

    First commit

---
+++ src/Interface/InterfaceExample.java
@@ -0,0 +1,48 @@
+package Interface;
+
+interface WaterBottle {
+    String color = "Blue";
+
+    void fill();
+
+    void pour();
+}
+
+interface MilkBottle {
+    String s = "White";
+
+    void coffee();
+
+    void mix();
+}
+
+public class InterfaceExample implements WaterBottle, MilkBottle {
+    public static void main(String[] args) {
+        InterfaceExample interfaceExample = new InterfaceExample();
+        System.out.println("Water");
+        System.out.println("Colour of the bottle is: " + interfaceExample.color);
+        interfaceExample.fill();
+        interfaceExample.pour();
+        System.out.println();
+        System.out.println("Coffee");
+        System.out.println("Colour of milk is: " + interfaceExample.s);
+        interfaceExample.coffee();
+        interfaceExample.mix();
+    }
+
+    public void fill() {
+        System.out.println("Fill clean water");
+    }
+
+    public void pour() {
+        System.out.println("Pour water");
+    }
+
+    public void coffee() {
+        System.out.println("Fill coffee powder");
+    }
+
+    public void mix() {
+        System.out.println("Mix coffee with milk");
+    }
+}
